fdf1d64fb325edee03b4e791b3d32b23ba7a87b2
oneyesoneno <oneyesoneno@gmail.com>
2017-05-18 17:03:24 +0700

    hadi on proses

--- translate/qa-lang-question.php
+++ translate/qa-lang-question.php
@@ -1,6 +1,6 @@
 <?php
 /*
-	Question2Answer by Gideon Greenspan and contributors
+	Hadi on proses Question2Answer by Gideon Greenspan and contributors
 	http://www.question2answer.org/
 
 	File: qa-include/qa-lang-question.php
